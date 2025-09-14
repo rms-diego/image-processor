cbc39324603822e16e61ecaa1aaa99568f42e4eb
Diego Ramos <diegorbrito9@gmail.com>
2025-09-14 17:46:36 -0300

    chore: create localstack setup

---
+++ .infra/init.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE
+  IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    password VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE
+  )
